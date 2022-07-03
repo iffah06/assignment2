deae68aba4b83f7a68d4cda5c988620ebd0d50e8
iffah06 <102519874+iffah06@users.noreply.github.com>
2022-07-03 15:14:10 +0800

    Create StudentView.java

---
+++ StudentView.java
@@ -0,0 +1,15 @@
+package assign2;
+
+public class StudentView {
+
+	public void printStudentMarks(int id, String name, String department, double math_mark,
+			double science_mark, double english_mark) {
+		System.out.println("Student id: "+id);
+		System.out.println("Name: "+name);
+		System.out.println("Department: "+department);
+		System.out.println("Mathematics: "+math_mark);
+		System.out.println("Science: "+science_mark);
+		System.out.println("English: "+english_mark);
+		
+	}
+}
